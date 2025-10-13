1ccec79647176ee6a5477fb7213eb8d4510018e3
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2025-10-13 19:36:50 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import './App.css'
+
+function App() {
+    return (
+    <Box>
+      <h1>Cat Blog</h1>
+    </Box>
+  )
+}
+
+export default App
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#FF6F00'
+        }, 
+        secondary: {
+            main: '#757575'
+        }
+    },
+    typography: {
+        fontFamily: 'Roboto, Arial, san-serif'
+    }
+})
